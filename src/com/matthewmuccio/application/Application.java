2f43513ac07009f552e6c629c340dfcfd3e06264
Matthew Muccio <me@matthewmuccio.com>
2016-10-21 01:33:03 -0400

    Began Application and Board classes.

--- src/com/matthewmuccio/application/Application.java
+++ src/com/matthewmuccio/application/Application.java
@@ -1,12 +1,29 @@
 package com.matthewmuccio.application;
 
+import java.awt.EventQueue;
 import javax.swing.JFrame;
 
+// Application is the entry point of the game.
 public class Application extends JFrame
 {
 	public Application()
 	{
 		
+	}
+	
+	private void initUI()
+	{
+		// A new Board is put in the center of the JFrame container.
+		this.add(new Board());
+		
+		// Sets properties of JFrame window.
+		this.setSize(250, 200);
+		this.setTitle("2D Game");
+		
+		// Application will close when user clicks on the close button. (Non-default behavior)
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Centers the JFrame window on the screen.
+		this.setLocationRelativeTo(null);
 	}
 	
 	public static void main(String[] args)
---
+++ src/com/matthewmuccio/application/Board.java
@@ -0,0 +1,12 @@
+package com.matthewmuccio.application;
+
+import javax.swing.JPanel;
+
+// Board is a JPanel where the game takes place.
+public class Board extends JPanel
+{
+	public Board()
+	{
+		
+	}
+}
