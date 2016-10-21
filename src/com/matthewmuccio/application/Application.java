aa9a880af8826dc0f88e19581e8d9d8c6277dd67
Matthew Muccio <me@matthewmuccio.com>
2016-10-21 01:26:39 -0400

    Began Application class.

---
+++ src/com/matthewmuccio/application/Application.java
@@ -0,0 +1,16 @@
+package com.matthewmuccio.application;
+
+import javax.swing.JFrame;
+
+public class Application extends JFrame
+{
+	public Application()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
